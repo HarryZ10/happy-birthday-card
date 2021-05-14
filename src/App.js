f01dbd3ebc9f85c3ad5f70ceedae9ef43a89ebaf
HarryZ10 <hzhu20@georgefox.edu>
2021-05-13 19:14:18 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+import Typer from './Typer';
+import AppFooter from './Footer';
+
+import titleCard from '../src/images/1.svg';
+import letter1 from '../src/images/2.svg';
+import letter2 from '../src/images/3.svg';
+import letter3 from '../src/images/4.svg';
+import letter4 from '../src/images/5.svg';
+import letter5 from '../src/images/6.svg';
+import letter6 from '../src/images/7.svg';
+
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+
+        {/* TITLE */}
+        <Grid spacing={3} xs={12} justify="flex-end" style={{position: 'absolute'}}>
+          <Grid item xs={12} sm={6}>
+            <Grid item xs={12} style={pageIntro}>
+              <h2 style={pageTitle}>
+                <Typer dataText={["Happy Birthday, Nico!!"]}/>
+              </h2>
+              <h3 style={mainSubTitleStyle}>
+                <Typer dataText={["You're the homie!!"]}/>
+              </h3>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <div style={{paddingTop: '2000px', paddingBottom: '100px'}}>
+        <Grid size={2}>
+          <Grid size={3} spacing={2}>
+            <Grid size={4} spacing={3}>
+              <Grid>
+                <img src={titleCard} class="stickyNote" className="container hoverable"/>
+              </Grid>
+                <img src={letter1} class="stickyNote" className="container hoverable"/>
+                <img src={letter2} class="stickyNote" className="container hoverable" />
+                <img src={letter3} class="stickyNote" className="container hoverable"/>
+                <img src={letter4} class="stickyNote" className="container hoverable"/>
+                <img src={letter5} class="stickyNote" className="container hoverable"/>
+                <img src={letter6} class="stickyNote" className="container hoverable"/>
+            </Grid>
+            </Grid>  
+            </Grid>
+
+        </div>
+       
+              
+           
+        <AppFooter>
+              <p>
+              © 2021 • Developed by Harry Zhu | Made with ReactJS
+              </p>
+            </AppFooter>
+      </div>
+    )
+  }
+}
+
+const pageIntro={
+  textAlign: 'left',
+  paddingTop: '50px',
+}
+
+
+const subTitleStyle = {
+  fontFamily: 'Comfortaa',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '14px',
+  lineHeight: 'normal',
+  padding: '0px',
+  display: 'flex',
+  alignItems: 'left',
+  textAlign: 'left',
+  transition: 'all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1)',
+  color: 'white',
+  justifyContent: 'left',
+}
+
+
+const mainSubTitleStyle = {
+  fontFamily: 'Comfortaa',
+  fontStyle: 'normal',
+  fontSize: '18px',
+  lineHeight: 'normal',
+  padding: '10px',
+  display: 'flex',
+  margin: 'auto',
+  alignItems: 'left',
+  textAlign: 'left',
+  color: '#353535',
+  justifyContent: 'left',
+  paddingBottom: '50px'
+}
+
+
+const pageTitle={
+  fontFamily: 'Abril Fatface', cursive: true,
+  fontStyle: 'normal',
+  fontSize: '36px',
+  lineHeight: 'normal',
+  margin: 'auto',
+  paddingTop: '500px',
+  marginTop: '100px',
+  display: 'flex',
+  justifyContent: 'left',
+  alignItems: 'left',
+  textAlign: 'left',
+  color: '#353535'
 }
 
 export default App;
---
+++ src/Typer.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+export default class Typer extends Component {
+
+    //  arguments to pass through Typer
+    static defaultProps = {
+      dataText: []
+    }
+  
+    // text, isDeleting, loop number of times, typing speed
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        text: '',
+        isDeleting: false,
+        loopNum: 0,
+        typingSpeed: 210
+      }
+    }
+  
+    // calls function to handle type
+    componentDidMount() {
+      this.handleType();
+    }
+  
+    // calculate typing effect
+    handleType = () => {
+      const { dataText } = this.props;
+      const { isDeleting, loopNum, text, typingSpeed } = this.state;
+      const i = loopNum % dataText.length;
+      const fullText = dataText[i];
+  
+      // speed of deleting text (turned off for now)
+      this.setState({
+        text: isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1),
+        typingSpeed: isDeleting ? 30 : 150
+      });
+  
+      // if text is empty due to deletion
+      if (isDeleting && text === '') {
+        
+        this.setState({
+          isDeleting: false,
+          loopNum: loopNum + 1
+        });
+        
+      }
+  
+      setTimeout(this.handleType, typingSpeed);
+    };
+  
+    render() {    
+      return this.state.text;
+      
+    }
+  }
+  
